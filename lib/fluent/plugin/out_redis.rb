d30986354db3d90e4dc69b91c846ca78120fba98
TrevorFoster <trevor.foster@hotmail.ca>
2016-10-11 04:34:46 -0400

    Added stuff to test with test kitchen.Started adding new configuration parameters to the output plugin.

--- lib/fluent/plugin/out_redis.rb
+++ lib/fluent/plugin/out_redis.rb
@@ -1,7 +1,18 @@
+require 'fluent/output'
+
 module Fluent
   class RedisOutput < BufferedOutput
     Fluent::Plugin.register_output('redis', self)
-    attr_reader :host, :port, :db_number, :redis
+
+    config_param :host, :string, :default => nil
+    config_param :socket_path, :string, :default => 'localhost'
+    config_param :port, :integer, :default => 6379
+    config_param :password, :string, :default => nil, :secret => true
+    config_param :db_number, :integer, :default => nil
+    config_param :data_type, :string, :default => 'key_value'
+    config_param :ttl, :integer, :default => nil
+    config_param :aggregate_operator, :string, :default => nil
+
 
     def initialize
       super
@@ -12,9 +23,9 @@ module Fluent
     def configure(conf)
       super
 
-      @host = conf.has_key?('host') ? conf['host'] : 'localhost'
-      @port = conf.has_key?('port') ? conf['port'].to_i : 6379
-      @db_number = conf.has_key?('db_number') ? conf['db_number'].to_i : nil
+      if @host.nil? && @socket_path.nil?
+        raise ConfigError, 'One of host or socket_path must be set to connect to redis server'
+      end
 
       if conf.has_key?('namespace')
         $log.warn "namespace option has been removed from fluent-plugin-redis 0.1.3. Please add or remove the namespace '#{conf['namespace']}' manually."
@@ -24,8 +35,18 @@ module Fluent
     def start
       super
 
-      @redis = Redis.new(:host => @host, :port => @port,
-                         :thread_safe => true, :db => @db_number)
+      opts = {:thread_safe => true, :db => @db_number}
+
+      opts[:password] = @password unless @password.nil?
+
+      if !@socket_path.nil?
+        opts[:path] = @socket_path
+      else
+        opts[:host] = @host
+        opts[:port] = @port
+      end
+
+      @redis = Redis.new(opts)
     end
 
     def shutdown
@@ -35,6 +56,18 @@ module Fluent
     def format(tag, time, record)
       identifier = [tag, time].join(".")
       [identifier, record].to_msgpack
+    end
+
+    def aggregate_chunk(chunk)
+      aggregate = Hash.new(0)
+
+      chunk.msgpack_each do
+
+      end
+    end
+
+    def aggregate_hm
+
     end
 
     def write(chunk)
